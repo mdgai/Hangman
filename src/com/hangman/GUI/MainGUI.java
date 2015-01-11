491606a1f9ec55314bf3a8bf7c3740611c7847f8
mdgai <mdgaintatzi@gmail.com>
2015-01-11 16:09:49 +0200

    Introduce classes AdminGUI and MainGUI
    Empty JFrames for the moment, they serve for demonstrating purposes
    only.

---
+++ src/com/hangman/GUI/MainGUI.java
@@ -0,0 +1,33 @@
+package com.hangman.GUI;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class MainGUI extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Create the frame.
+	 * 
+	 * @param title
+	 */
+	public MainGUI(String title) {
+		this.setTitle(title);
+		initialize();
+
+	}
+
+	public void initialize() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
