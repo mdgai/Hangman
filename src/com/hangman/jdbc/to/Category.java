a0751265eaf26d39a4e86f0eefa9e31f89633911
Alex <alexandratzan@gmail.com>
2015-01-14 14:29:59 +0200

    Category Class

---
+++ src/com/hangman/jdbc/to/Category.java
@@ -0,0 +1,24 @@
+package com.hangman.jdbc.to;
+
+public class Category {
+
+	private String CategoryName;
+	private int CategoryId;
+
+	public String getCategoryName() {
+		return CategoryName;
+	}
+
+	public void setUsername(String CategoryName) {
+		this.CategoryName = CategoryName;
+	}
+
+	public int getCategoryId() {
+		return CategoryId;
+	}
+
+	public void setCategoryId(int id) {
+		this.CategoryId = id;
+	}
+
+}
