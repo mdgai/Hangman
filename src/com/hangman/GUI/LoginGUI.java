ce14f04214de4c9301ed77c4b2b893843c3ed555
mdgai <mdgaintatzi@gmail.com>
2015-01-17 19:19:58 +0200

    Add JLabel create account in the LoginGUI form
    Add JLabel create account and the corresponding MouseListener.
    On click this label leads to JDialog SubmitUserGUI.

--- src/com/hangman/GUI/LoginGUI.java
+++ src/com/hangman/GUI/LoginGUI.java
@@ -3,6 +3,8 @@ package com.hangman.GUI;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -11,6 +13,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 import com.hangman.jdbc.service.UserService;
 
@@ -58,12 +61,39 @@ public class LoginGUI {
 				.getResource("/com/hangman/data/hangman2.JPG")));
 		panel.add(ImageLabel);
 
+		// Add label create account
+		final JLabel lblCreateAccount = new JLabel("create account");
+		lblCreateAccount.setHorizontalAlignment(SwingConstants.RIGHT);
+
+		// Add a MouseListener to the label create account
+
+		lblCreateAccount.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseEntered(MouseEvent evt) {
+				lblCreateAccount.setForeground(Color.white);
+			}
+
+			@Override
+			public void mouseExited(MouseEvent evt) {
+				lblCreateAccount.setForeground(Color.black);
+			}
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				new SubmitUserGUI().setVisible(true);
+			}
+		});
+
+		lblCreateAccount.setBounds(252, 285, 165, 20);
+		panel.add(lblCreateAccount);
+
 		JLabel lblUsername = new JLabel("Username");
 		lblUsername.setBounds(115, 184, 70, 14);
 		panel.add(lblUsername);
 
 		JLabel lblPassword = new JLabel("Password");
-		lblPassword.setBounds(115, 224, 70, 14);
+		lblPassword.setBounds(115, 209, 70, 14);
 		panel.add(lblPassword);
 
 		// creates textFields
@@ -75,7 +105,7 @@ public class LoginGUI {
 
 		final JPasswordField passwordField = new JPasswordField();
 		passwordField.setText("guest");
-		passwordField.setBounds(195, 221, 108, 20);
+		passwordField.setBounds(195, 206, 108, 20);
 		panel.add(passwordField);
 		passwordField.setColumns(10);
 
@@ -107,7 +137,6 @@ public class LoginGUI {
 						String message = "Invalid username or password. "
 								+ "Please try again.";
 
-						
 						DialogHelper.showError(frmHandman, message, "Hangman");
 
 					}
@@ -116,7 +145,7 @@ public class LoginGUI {
 				}
 			}
 		});
-		btnEnter.setBounds(171, 270, 89, 23);
+		btnEnter.setBounds(163, 242, 89, 23);
 		panel.add(btnEnter);
 
 	}
