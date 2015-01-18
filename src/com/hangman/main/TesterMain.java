bdaf59e0bc29ba6e3c878b67ce63ede450752161
TMargaret <tmargaret@hotmail.com>
2015-01-18 12:54:22 +0200

    testerMain

---
+++ src/com/hangman/main/TesterMain.java
@@ -0,0 +1,13 @@
+package com.hangman.main;
+
+//import com.hangman.guiMainFrame.HangmanMainFrame;
+
+public class TesterMain {
+
+	public static void main(String[] args) {
+
+		final String word = "Giraffe";
+
+		//new HangmanMainFrame(word);
+	}
+}
