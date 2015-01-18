5e6503b3fca6a2e83b576076a1e2ef2eaa536b6f
mdgai <mdgaintatzi@gmail.com>
2015-01-18 17:54:51 +0200

    Modify class SubmitUserGUI
    Handle validation of the form, limit password fields to ten characters,
    add icons

--- src/com/hangman/GUI/SubmitUserGUI.java
+++ src/com/hangman/GUI/SubmitUserGUI.java
@@ -15,6 +15,10 @@ import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
+import javax.swing.text.PlainDocument;
 
 import net.miginfocom.swing.MigLayout;
 
@@ -26,22 +30,23 @@ public class SubmitUserGUI extends JDialog {
 	private final JPanel contentPanel = new JPanel();
 	private JTextField txtUsername;
 	private JPasswordField fldPassword;
-	private JPasswordField fldPassword_1;
+	private JPasswordField fldPasswordMatch;
 	private JTextField txtFirstname;
 	private JTextField txtLastname;
 	private JLabel lblUsername;
 	private JLabel lblPassword;
-	private JLabel lblPassword_1;
+	private JLabel lblPasswordMatch;
 	private JLabel lblFirstName;
 	private JLabel lblLastName;
 	private JButton saveButton;
 	private JButton cancelButton;
 	private JLabel lblUsernameErr;
 	private JLabel lblPasswordErr;
-	private JLabel lblPassword_1Err;
+	private JLabel lblPasswordMatchErr;
 	private JLabel lblFirstnameErr;
 	private JLabel lblLastnameErr;
 	private JPanel buttonPane;
+	private PlainDocument document;
 	private ImageIcon exclamation = new ImageIcon(getClass().getResource(
 			"/com/hangman/data/exclamation.png"));
 	private ImageIcon tick = new ImageIcon(getClass().getResource(
@@ -57,17 +62,20 @@ public class SubmitUserGUI extends JDialog {
 
 	private void initialize() {
 		setModal(true);
+		setTitle("Create Account");
 		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
+		contentPanel.isFocusCycleRoot();
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		getContentPane().add(contentPanel, BorderLayout.NORTH);
 		contentPanel.setLayout(new MigLayout("",
-				"[right][100px,grow][100px][][][][]", "[][][][][][]"));
+				"[120px,grow][200px,grow][100px][][][][]",
+				"[26][26][26][26][26][26]"));
 
 		lblUsername = new JLabel("Username *");
-		contentPanel.add(lblUsername, "cell 0 0,alignx trailing");
+		contentPanel.add(lblUsername, "cell 0 0,alignx right");
 
 		txtUsername = new JTextField();
 		contentPanel.add(txtUsername, "cell 1 0,growx");
@@ -77,7 +85,7 @@ public class SubmitUserGUI extends JDialog {
 		contentPanel.add(lblUsernameErr, "cell 2 0");
 
 		lblPassword = new JLabel("Password *");
-		contentPanel.add(lblPassword, "cell 0 1,alignx trailing");
+		contentPanel.add(lblPassword, "cell 0 1,alignx right");
 
 		fldPassword = new JPasswordField(10);
 		contentPanel.add(fldPassword, "cell 1 1,growx");
@@ -85,20 +93,20 @@ public class SubmitUserGUI extends JDialog {
 		lblPasswordErr = new JLabel("");
 		contentPanel.add(lblPasswordErr, "cell 2 1");
 
-		lblPassword_1 = new JLabel("Password *");
-		contentPanel.add(lblPassword_1, "cell 0 2,alignx trailing");
+		lblPasswordMatch = new JLabel("Password *");
+		contentPanel.add(lblPasswordMatch, "cell 0 2,alignx right");
 
-		lblPassword_1Err = new JLabel("");
-		contentPanel.add(lblPassword_1Err, "cell 2 2");
+		lblPasswordMatchErr = new JLabel("");
+		contentPanel.add(lblPasswordMatchErr, "cell 2 2");
 
-		fldPassword_1 = new JPasswordField(10);
-		contentPanel.add(fldPassword_1, "cell 1 2,growx");
+		fldPasswordMatch = new JPasswordField(10);
+		contentPanel.add(fldPasswordMatch, "cell 1 2,growx");
 
 		lblFirstnameErr = new JLabel("");
 		contentPanel.add(lblFirstnameErr, "cell 2 3");
 
 		lblFirstName = new JLabel("First name *");
-		contentPanel.add(lblFirstName, "cell 0 3,alignx trailing");
+		contentPanel.add(lblFirstName, "cell 0 3,alignx right");
 
 		txtFirstname = new JTextField();
 		contentPanel.add(txtFirstname, "cell 1 3,growx");
@@ -108,7 +116,7 @@ public class SubmitUserGUI extends JDialog {
 		contentPanel.add(lblLastnameErr, "cell 2 4");
 
 		lblLastName = new JLabel("Last Name *");
-		contentPanel.add(lblLastName, "cell 0 4,alignx trailing");
+		contentPanel.add(lblLastName, "cell 0 4,alignx right");
 
 		txtLastname = new JTextField();
 		contentPanel.add(txtLastname, "cell 1 4,growx");
@@ -133,6 +141,45 @@ public class SubmitUserGUI extends JDialog {
 			cancelButton.setActionCommand("Cancel");
 			buttonPane.add(cancelButton);
 		}
+
+		/**
+		 * Limits the password to ten characters
+		 */
+		document = (PlainDocument) fldPassword.getDocument();
+		document.setDocumentFilter(new DocumentFilter() {
+
+			@Override
+			public void replace(DocumentFilter.FilterBypass fb, int offset,
+					int length, String text, AttributeSet attrs)
+					throws BadLocationException {
+				String string = fb.getDocument().getText(0,
+						fb.getDocument().getLength())
+						+ text;
+
+				if (string.length() <= 10) {
+					super.replace(fb, offset, length, text, attrs);
+				}
+			}
+
+		});
+
+		document = (PlainDocument) fldPasswordMatch.getDocument();
+		document.setDocumentFilter(new DocumentFilter() {
+
+			@Override
+			public void replace(DocumentFilter.FilterBypass fb, int offset,
+					int length, String text, AttributeSet attrs)
+					throws BadLocationException {
+				String string = fb.getDocument().getText(0,
+						fb.getDocument().getLength())
+						+ text;
+
+				if (string.length() <= 10) {
+					super.replace(fb, offset, length, text, attrs);
+				}
+			}
+
+		});
 	}
 
 	// Private method that includes all events of the window.
@@ -148,7 +195,7 @@ public class SubmitUserGUI extends JDialog {
 					UserService userService = new UserService();
 
 					insertUser.setUsername(txtUsername.getText());
-					insertUser.setPassword(fldPassword.getSelectedText());
+					insertUser.setPassword(new String(fldPassword.getPassword()));
 					insertUser.setFirstName(txtFirstname.getText());
 					insertUser.setLastName(txtLastname.getText());
 					insertUser.setAdmin(false);
@@ -169,71 +216,56 @@ public class SubmitUserGUI extends JDialog {
 		});
 
 		// *********************************************************
-		// event for losing focus of text field NAME.
+		// event for losing focus of text field Username.
 		// *********************************************************
 		txtUsername.addFocusListener(new FocusAdapter() {
 			@Override
 			public void focusLost(FocusEvent e) {
-				if (txtUsername.getText().isEmpty())
-					lblUsernameErr.setIcon(exclamation);
-				else
-					lblUsernameErr.setIcon(tick);
+				fillTextField(txtUsername, lblUsernameErr);
 			}
 		});
 
 		// *********************************************************
-		// event for losing focus of text field NAME.
+		// event for losing focus of text field PasswordMatch.
 		// *********************************************************
-		fldPassword.addFocusListener(new FocusAdapter() {
+		fldPasswordMatch.addFocusListener(new FocusAdapter() {
 			@Override
 			public void focusLost(FocusEvent e) {
-				String password = new String(fldPassword.getPassword());
-				if (password.isEmpty()) {
-					lblPasswordErr.setIcon(exclamation);
-				} else
-					lblPasswordErr.setIcon(tick);
+
+				String password = new String(fldPasswordMatch.getPassword());
+				String match = new String(fldPassword.getPassword());
+
+				if (!match.isEmpty()) {
+					if (password.equalsIgnoreCase(match))
+						lblPasswordMatchErr.setIcon(new ImageIcon(
+								SubmitUserGUI.class
+										.getResource("/com/hangman/data/tick_circle.png")));
+					else
+						lblPasswordMatchErr.setIcon(new ImageIcon(
+								SubmitUserGUI.class
+										.getResource("/com/hangman/data/exclamation.png")));
+				}
+
 			}
 		});
 
 		// *********************************************************
-		// event for losing focus of text field NAME.
-		// *********************************************************
-		fldPassword_1.addFocusListener(new FocusAdapter() {
-			@Override
-			public void focusLost(FocusEvent e) {
-				String password = new String(fldPassword_1.getPassword());
-				if (!password.isEmpty()
-						&& password.equalsIgnoreCase(new String(fldPassword
-								.getPassword())))
-					lblPassword_1Err.setIcon(tick);
-				else
-					lblPassword_1Err.setIcon(exclamation);
-			}
-		});
-
-		// *********************************************************
-		// event for losing focus of text field NAME.
+		// event for losing focus of text field FirstName.
 		// *********************************************************
 		txtFirstname.addFocusListener(new FocusAdapter() {
 			@Override
 			public void focusLost(FocusEvent e) {
-				if (txtFirstname.getText().isEmpty())
-					lblFirstnameErr.setIcon(exclamation);
-				else
-					lblFirstnameErr.setIcon(tick);
+				fillTextField(txtFirstname, lblFirstnameErr);
 			}
 		});
 
 		// *********************************************************
-		// event for losing focus of text field NAME.
+		// event for losing focus of text field LastName.
 		// *********************************************************
 		txtLastname.addFocusListener(new FocusAdapter() {
 			@Override
 			public void focusLost(FocusEvent e) {
-				if (txtLastname.getText().isEmpty())
-					lblLastnameErr.setIcon(exclamation);
-				else
-					lblLastnameErr.setIcon(tick);
+				fillTextField(txtLastname, lblLastnameErr);
 			}
 		});
 
@@ -247,16 +279,66 @@ public class SubmitUserGUI extends JDialog {
 	 */
 	private boolean validateForm() {
 		Boolean isOk = true;
-		if ((txtUsername.getText().isEmpty())
-				|| new String(fldPassword.getPassword()).isEmpty()
-				|| new String(fldPassword_1.getPassword()).isEmpty()
-				|| txtFirstname.getText().isEmpty()
-				|| txtLastname.getText().isEmpty()
-				|| !new String(fldPassword_1.getPassword())
-						.equalsIgnoreCase(new String(fldPassword.getPassword())))
+		String msg = "Field(s) missing: ";
+		if (txtUsername.getText().isEmpty()) {
 			isOk = false;
+			msg = msg + "username";
+		}
+		if (new String(fldPassword.getPassword()).isEmpty()
+				|| new String(fldPasswordMatch.getPassword()).isEmpty()) {
+			if (isOk)
+				msg = msg + " password";
+			else
+				msg = msg + ", password";
+			isOk = false;
+		}
+		if (txtFirstname.getText().isEmpty()) {
+			if (isOk)
+				msg = msg + " first name";
+			else
+				msg = msg + ", first name";
+			isOk = false;
+		}
+		if (txtLastname.getText().isEmpty()) {
+			if (isOk)
+				msg = msg + " last name.";
+			else
+				msg = msg + " and last name.";
+			isOk = false;
+		}
+		if (!new String(fldPassword.getPassword()).isEmpty()
+				&& !new String(fldPasswordMatch.getPassword()).isEmpty()
+				&& !new String(fldPasswordMatch.getPassword())
+						.equalsIgnoreCase(new String(fldPassword.getPassword()))) {
+			if (isOk)
+				msg = msg + " none. \n Passwords do not match";
+			else
+				msg = msg + "\n Passwords do not match";
+			isOk = false;
+		}
 
-		return isOk;
+		if (isOk == false) {
+			DialogHelper.showWarning(contentPanel, msg, "Something went wrong");
+			return isOk;
+		} else
+			return isOk;
 	}
 
+	/**
+	 * This method checks if the text field is empty. If it is, it shows an
+	 * exclamation mark, if it's not it shows a tick mark.
+	 * 
+	 * @param textField
+	 * @param labelErr
+	 * @author Maria - Despoina Gkaintatzi
+	 */
+	private void fillTextField(JTextField textField, JLabel labelErr) {
+		if (textField.getText().isEmpty()) {
+			labelErr.setIcon(new ImageIcon(SubmitUserGUI.class
+					.getResource("/com/hangman/data/exclamation.png")));
+		} else
+			labelErr.setIcon(new ImageIcon(SubmitUserGUI.class
+					.getResource("/com/hangman/data/tick_circle.png")));
+
+	}
 }
